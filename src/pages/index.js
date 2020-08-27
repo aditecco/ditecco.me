26891d6831ec31ce12837ff0248bd9cbed15fce6
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-28 01:23:18 +0200

    WIP homepage re-implementation in Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,32 +5,118 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import cards from "../data/cards"
-
 import "../styles/home.scss"
 
 export default function IndexPage() {
   return (
     <>
-      <SEO title="Home" />
+      <SEO title="ditecco.me | Home" />
 
       <main className="card-list-container">
         <ul className="card-list">
-          {cards.map(card => (
-            <li className="card-list-item">
-              <a href={card.href}>
-                <article className="card-list-item-body">
-                  <h5>{card.subtitle}</h5>
-                  <h3>{card.title}</h3>
-                  <p>{card.body}</p>
+          {cards.map(card => {
+            const {
+              master,
+              expanded,
+              href,
+              title,
+              subtitle,
+              body,
+              tags,
+              heroImg,
+              heroAlt,
+            } = card
 
-                  {card.tags.length &&
-                    card.tags.map(tag => (
-                      <span className="card-list-item-tag">{tag}</span>
-                    ))}
-                </article>
-              </a>
-            </li>
-          ))}
+            return (
+              <li
+                className={`card-list-item${
+                  master
+                    ? " card-list-item--master card-list-item--3rows"
+                    : expanded
+                    ? " card-list-item--2rows"
+                    : ""
+                }`}
+              >
+                <a href={href}>
+                  <article>
+                    {master ? (
+                      <div className="card-list-item-hero">
+                        <h2>
+                          Hi, I'm Alessandro
+                          <br />
+                          Di Tecco
+                        </h2>
+                      </div>
+                    ) : expanded ? (
+                      <div className="card-list-item-hero">
+                        <img src={heroImg} alt={heroAlt} />
+                      </div>
+                    ) : null}
+
+                    <div className="card-list-item-body">
+                      {master && (
+                        <>
+                          <p>
+                            Hi there! I'm Alessandro Di Tecco, front-end
+                            developer.
+                          </p>
+
+                          <p>
+                            I love the open web and I'm passionate about
+                            building products &amp; experiences with code.
+                          </p>
+
+                          <p>
+                            This website is a collection of my most significant
+                            works and projects.
+                          </p>
+
+                          <p>
+                            To get in touch with me you can{" "}
+                            <a href="mailto:&#x61;&#x6C;&#x65;&#x73;&#x73;&#x61;&#x6E;&#x64;&#x72;&#x6F;&#x40;&#x64;&#x69;&#x74;&#x65;&#x63;&#x63;&#x6F;&#x2E;&#x6D;&#x65;">
+                              send me an email
+                            </a>{" "}
+                            (old fashioned!)
+                          </p>
+                        </>
+                      )}
+
+                      {subtitle && <h5>{subtitle}</h5>}
+                      {title && <h3>{title}</h3>}
+                      {body && <p>{body}</p>}
+
+                      {tags.length
+                        ? tags.map(tag => (
+                            <span className="card-list-item-tag">{tag}</span>
+                          ))
+                        : null}
+                    </div>
+
+                    {master && (
+                      <div className="card-list-item-footer">
+                        <h6>Find me also on:</h6>
+                        <ul className="contact-list">
+                          <li>
+                            <a href="mailto:&#x61;&#x6C;&#x65;&#x73;&#x73;&#x61;&#x6E;&#x64;&#x72;&#x6F;&#x40;&#x64;&#x69;&#x74;&#x65;&#x63;&#x63;&#x6F;&#x2E;&#x6D;&#x65;">
+                              Email
+                            </a>
+                          </li>
+                          <li>&middot;</li>
+                          <li>
+                            <a href="https://gitlab.com/aditecco">GitLab</a>
+                          </li>
+                          <li>&middot;</li>
+                          <li>
+                            <a href="https://twitter.com/aditecco">Twitter</a>
+                          </li>
+                        </ul>
+                      </div>
+                    )}
+                  </article>
+                </a>
+              </li>
+            )
+          })}
         </ul>
       </main>
     </>
