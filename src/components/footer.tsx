6360779e1e88bfa33178faa62b9e4974f64b2142
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-28 23:20:33 +0200

    Updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -29,7 +29,10 @@ export default function Footer(props: IOwnProps): ReactElement {
       <small>
         &copy; {site?.siteMetadata?.currentYear} {site?.siteMetadata?.author},
         All rights reserved. &mdash; v.
-        {site?.siteMetadata?.version} &bull; {site?.buildTime}
+        {site?.siteMetadata?.version} &bull; {site?.buildTime} &bull; made with{" "}
+        <a href="https://www.gatsbyjs.com/" target="_blank">
+          Gatsby
+        </a>
       </small>
     </footer>
   )
