afd373d5a4b52935ce02f1534c878921dd850947
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-28 16:59:00 +0200

    Added boilerplate for programmatic page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,36 @@
  */
 
 // You can delete this file if you're not using it
+
+// exports.onCreateNode = ({ node }) => {
+// }
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const result = await graphql(`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     console.log(node)
+
+//     createPage({
+//       path: node.fields.slug,
+//       component: path.resolve(`./src/templates/blog-post.js`),
+//       context: {
+//         // Data passed to context is available
+//         // in page queries as GraphQL variables.
+//         slug: node.fields.slug,
+//       },
+//     })
+//   })
+// }
