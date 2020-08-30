b964a29e16e87df38e212144ba40e7253f4211dc
Alessandro Di Tecco <alessandro@ditecco.me>
2020-08-30 03:11:05 +0200

    Constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+/* ---------------------------------
+constants
+--------------------------------- */
+
+export const TWITTER_URL = "https://twitter.com/aditecco"
+export const GITLAB_URL = "https://gitlab.com/aditecco"
+export const ENCODED_EMAIL =
+  "mailto:&#x61;&#x6C;&#x65;&#x73;&#x73;&#x61;&#x6E;&#x64;&#x72;&#x6F;&#x40;&#x64;&#x69;&#x74;&#x65;&#x63;&#x63;&#x6F;&#x2E;&#x6D;&#x65;"
