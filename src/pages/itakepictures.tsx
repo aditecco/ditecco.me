b0a13740d52a9ce7f7e18570c021f278d20d3053
Alessandro Di Tecco <alessandro@ditecco.me>
2020-09-05 17:09:28 +0200

    Recreated basic look & feel

--- src/pages/itakepictures.tsx
+++ src/pages/itakepictures.tsx
@@ -14,46 +14,70 @@ interface IGatsbyProps {
 }
 
 interface IGraphQLQueryResponseNode {
-  node: {
-    id: string
-    timeToRead: number
-    excerpt: string
-    fields: {
-      slug: string
-    }
-    frontmatter: {
-      title: string
-      subtitle: string | null
-      language: string
-      timestamp: string
-      author: string
-      tags: string
-    }
-  }
+  node: {}
 }
 
 type TProps = IOwnProps & IGatsbyProps
 
 export default function ITakePictures({ data }: TProps): ReactElement {
-  // const {
-  //   allMarkdownRemark: { edges: posts },
-  // } = data
-  console.log(data)
+  const x = [
+    "itp-ph-01-sq.jpg",
+    "itp-ph-02-sq.jpg",
+    "itp-ph-02-wd.jpg",
+    "itp-ph-03-sq.jpg",
+    "itp-ph-03-wd.jpg",
+    "itp-ph-04-sq.jpg",
+    "itp-ph-04-wd.jpg",
+    "itp-ph-05-sq.jpg",
+    "itp-ph-05-wd.jpg",
+    "itp-ph-06-sq.jpg",
+    "itp-ph-06-wd.jpg",
+    "itp-ph-07-sq.jpg",
+    "itp-ph-07-wd.jpg",
+    "itp-ph-08-sq.jpg",
+    "itp-ph-08-wd.jpg",
+    "itp-ph-09-sq.jpg",
+    "itp-ph-09-wd.jpg",
+    "itp-ph-10-sq.jpg",
+    "itp-ph-10-wd.jpg",
+    "itp-ph-11-sq.jpg",
+    "itp-ph-11-wd.jpg",
+    "itp-ph-12-sq.jpg",
+    "itp-ph-12-wd.jpg",
+    "itp-ph-13-sq.jpg",
+    "itp-ph-13-wd.jpg",
+    "itp-ph-14-wd.jpg",
+    "itp-ph-15-wd.jpg",
+    "itp-ph-16-wd.jpg",
+    "itp-ph-17-wd.jpg",
+    "itp-ph-18-wd.jpg",
+    "itp-ph-19-wd.jpg",
+  ]
 
   return (
     <div className="wrapper">
       <header className="header">
-        <h1>I take pictures.</h1>
-        <h2>photos by Alessandro Di Tecco.</h2>
+        <h1 className="title">I take pictures.</h1>
+        <h5 className="subtitle">photos by Alessandro Di Tecco.</h5>
       </header>
 
       <div className="container">
         {data.allFile.edges.map(image => (
-          <div className="module">
-            <Link to={"/projects"}>
-              <Img fixed={image.node.childImageSharp.fixed} fadeIn alt="" />
+          <div
+            className={
+              image.node.childImageSharp.fluid.originalName.includes("-wd")
+                ? "module full"
+                : "module half"
+            }
+          >
+            {/* TODO link to detail page */}
+            <Link
+              to={"/projects"}
+              state={{ from: image.node.childImageSharp.fluid.originalName }}
+            >
+              <Img fluid={image.node.childImageSharp.fluid} fadeIn alt="" />
 
-              <div className="capt">
+              <div className="caption">
                 <p>Grass</p>
               </div>
             </Link>
@@ -70,8 +94,9 @@ export const query = graphql`
       edges {
         node {
           childImageSharp {
-            fixed {
-              ...GatsbyImageSharpFixed
+            fluid {
+              originalName
+              ...GatsbyImageSharpFluid
             }
           }
         }
