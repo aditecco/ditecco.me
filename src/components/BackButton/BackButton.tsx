8564406bf1a9da3fc0bb7ccd8e92872f41c5b41f
Alessandro Di Tecco <alessandro@ditecco.me>
2020-09-06 03:49:30 +0200

    BackButton fine-tuning

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -2,18 +2,23 @@
 BackButton
 --------------------------------- */
 
-import React, { ReactElement } from "react"
+import React, { ReactElement, CSSProperties } from "react"
 import { Link } from "gatsby"
 import "./BackButton.scss"
 
 interface IOwnProps {
   target: string
   label?: string
+  style?: CSSProperties
 }
 
-export default function BackButton({ target, label }: IOwnProps): ReactElement {
+export default function BackButton({
+  target,
+  label,
+  style,
+}: IOwnProps): ReactElement {
   return (
-    <div className="BackButton">
+    <div className="BackButton" style={style || {}}>
       <i className="material-icons">keyboard_backspace</i>
       <Link to={target}>
         <span className="label">{label || "Back"}</span>
