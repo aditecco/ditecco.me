40f8148ca1e252358ba478caa6517d0267a9f4e6
Alessandro Di Tecco <alessandro@ditecco.me>
2020-09-14 01:35:27 +0200

    Limited slug creation to blog posts only

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,17 +15,20 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
-  // TODO execute only on /blog posts
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `content` })
+  if (
+    node.internal.type === `MarkdownRemark` &&
+    node.sourceInstanceName === "blog"
+  ) {
+    const slug = createFilePath({ node, getNode, basePath: `content/blog/` })
 
+    // blog posts will be conventionally named
+    // using this format: DD-MM-YYYY--title
     const slugParts = slug.split("--")
 
     createNodeField({
       node,
       name: `slug`,
-      value:
-        "/blog/" + slugParts.shift().match(/[\d]{4}/g) + "/" + slugParts.pop(),
+      value: slugParts.shift().match(/[\d]{4}/g) + "/" + slugParts.pop(),
     })
   }
 }
